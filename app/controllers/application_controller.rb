8ef77556101fd5e1b1213b0e9fd54120a9780c65
yukihirasawa1206 <h_x04yjr0@yahoo.co.jp>
2019-02-08 10:42:38 +0900

    add environment variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+	CONSUMER_KEY = ENV['CONSUMER_KEY']
+	CONSUMER_SECRET = ENV['CONSUMER_SECRET']
+	CALLBACK_URL = 'https://api.zaim.net/v2/'
+	API_URL = 'https://api.zaim.net/v2/'
 end
