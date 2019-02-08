76444ced1816db96853cb4b4c13b965bf294589a
yukihirasawa1206 <h_x04yjr0@yahoo.co.jp>
2019-02-08 16:28:42 +0900

    add account books routing

--- app/controllers/account_books_controller.rb
+++ app/controllers/account_books_controller.rb
@@ -1,6 +1,9 @@
 class AccountBooksController < ApplicationController
 
 	def index
+		payment_records_json = @access_token.get("#{API_URL}home/money")
+		@payment_records = JSON.parse(payment_record_json.body)
+		binding.pry
 	end
 
 	def create
