63a6b5afc8d579379f890567813eeea7b810aed8
yukihirasawa1206 <h_x04yjr0@yahoo.co.jp>
2019-02-14 23:20:24 +0900

    add request spec

--- spec/requests/account_books_spec.rb
+++ spec/requests/account_books_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "AccountBooks", type: :request do
         headers: { 'Content-Type'=> 'application/json' })
     end
 
-    it "responds successfully" do
+    it "should respond 200 status" do
       get account_books_path
       expect(response).to have_http_status(200)
     end
@@ -34,13 +34,38 @@ RSpec.describe "AccountBooks", type: :request do
         headers: { 'Content-Type'=> 'application/json' })
     end
 
-    it "responds successfully" do
+    it "should respond success message with valid account_data" do
       account_data_attributes = FactoryBot.attributes_for(:account_data)
       post account_books_path, params:{account_data: account_data_attributes}
+      expect(flash[:success]).to include("create account record is successful.")
       expect(response).to have_http_status(302)
       expect(response).to redirect_to account_books_path
     end
 
+    it "should respond alert message with invalid account_data" do
+      invalid_data_attributes= FactoryBot.attributes_for(:account_data, category_id:"")
+      post account_books_path, params:{ account_data: invalid_data_attributes }
+      expect(flash[:alert]).to include("Input value is incorrect")
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to account_books_path
+    end
+
+  end
+
+	describe "should be redirect to #index after delete account data" do
+    before do
+      allow_any_instance_of(AccountBooksController).to receive(:authenticated_user)
+      WebMock.enable!
+      WebMock.stub_request(:delete, 'https://api.zaim.net/v2/home/money/payment/1').to_return(
+        status: 200,
+        headers: { 'Content-Type'=> 'application/json' })
+    end
+    it "should respond success message after delete account record" do
+      delete account_book_path(1)
+      expect(response).to have_http_status(302)
+      expect(flash[:success]).to include("destroy account record is successful")
+      expect(response).to redirect_to account_books_path
+    end
   end
 
 end
